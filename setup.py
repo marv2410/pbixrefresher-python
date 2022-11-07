f5773936fa0582e6f6588ca810df6969ba6c8d52
marv2410 <god.bless.ec@gmail.com>
2022-11-07 12:28:32 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='pbixrefresher',
       version='0.1.8',
       description='Script for refreshing and publishing Power BI workbooks',
-      url='https://github.com/dubravcik/pbixrefresher-python',
-      author='Michal Dubravcik',
-      author_email='michal.dubravcik@gmail.com',
+      url='https://github.com/marv2410/pbixrefresher-python',
+      author='mro',
+      author_email='god.bless.ec@gmail.com',
       license='MIT',
       packages=['pbixrefresher'],
       install_requires=[
